6abef45a38d42613b55076d8c7c58de37161e183
kamendov-maxim <kamendov.maksim@icloud.com>
2023-09-30 11:19:58 +0300

    1

--- semester_1/homework1/substring.c
+++ semester_1/homework1/substring.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int countingSubstrings(char *string, char *subString, int lengthOfString, int lengthOfSubstring);
+int countSubstrings(const char *string, const char *subString, int lengthOfString, int lengthOfSubstring);
 int readingLineFromConsole(char *string);
 bool test(void);
 
@@ -13,18 +13,18 @@ int main()
         return 1;
     }
     printf("Enter your string\n");
-    char S[1000];
-    int lengthOfS = readingLineFromConsole(S);
+    char s[1000] = { '\0' };
+    int lengthOfS = readingLineFromConsole(s);
     printf("Enter your substring\n");
-    char S1[1000];
-    int lengthOfS1 = readingLineFromConsole(S1);
-    int numberOfSubstrings = countingSubstrings(S, S1, lengthOfS, lengthOfS1);
+    char s1[1000] = { '\0' };
+    int lengthOfS1 = readingLineFromConsole(s1);
+    int numberOfSubstrings = countSubstrings(s, s1, lengthOfS, lengthOfS1);
 
     printf("The number of occurances of your substring in your string is %d\n", numberOfSubstrings);
     return 0;
 }
 
-int countingSubstrings(char *string, char *subString, int lengthOfString, int lengthOfSubstring)
+int countSubstrings(const char *string, const char *subString, int lengthOfString, int lengthOfSubstring)
 {
     int count = 0;
     bool doWeCompareRightNow = false;
@@ -35,9 +35,10 @@ int countingSubstrings(char *string, char *subString, int lengthOfString, int le
     {
         if (doWeCompareRightNow)
         {
-            if (++currentSubstringCharacter < lengthOfSubstring)
+            ++currentSubstringCharacter;
+            if (currentSubstringCharacter < lengthOfSubstring)
             {
-                doWeCompareRightNow = (string[currentCharacter] == subString[currentSubstringCharacter] ? true : false);
+                doWeCompareRightNow = string[currentCharacter] == subString[currentSubstringCharacter];
             }
 
             if (!doWeCompareRightNow)
@@ -65,7 +66,7 @@ int countingSubstrings(char *string, char *subString, int lengthOfString, int le
 int readingLineFromConsole(char *string)
 {
     int c = 0;
-    char ch;
+    char ch = "\0";
     do
     {
         ch = getchar();
@@ -86,7 +87,7 @@ bool test(void)
     int testStringLength1 = 0;
     int testSubStringLength1 = 6;
     int answer1 = 0;
-    if (!(countingSubstrings(testString1, testSubstring1, testStringLength1, testSubStringLength1) == answer1))
+    if (!(countSubstrings(testString1, testSubstring1, testStringLength1, testSubStringLength1) == answer1))
     {
         printf("%d", 1);
         return false;
@@ -97,7 +98,7 @@ bool test(void)
     int testStringLength2 = 23;
     int testSubStringLength2 = 3;
     int answer2 = 4;
-    if (!(countingSubstrings(testString2, testSubstring2, testStringLength2, testSubStringLength2) == answer2))
+    if (!(countSubstrings(testString2, testSubstring2, testStringLength2, testSubStringLength2) == answer2))
     {
         return false;
     }
@@ -107,7 +108,7 @@ bool test(void)
     int testStringLength3 = 9;
     int testSubStringLength3 = 0;
     int answer3 = 0;
-    if (!(countingSubstrings(testString3, testSubstring3, testStringLength3, testSubStringLength3) == answer3))
+    if (!(countSubstrings(testString3, testSubstring3, testStringLength3, testSubStringLength3) == answer3))
     {
         return false;
     }
@@ -116,7 +117,7 @@ bool test(void)
     int testStringLength4 = 5;
     int testSubStringLength4 = 1;
     int answer4 = 5;
-    if (!(countingSubstrings(testString4, testSubstring4, testStringLength4, testSubStringLength4) == answer4))
+    if (!(countSubstrings(testString4, testSubstring4, testStringLength4, testSubStringLength4) == answer4))
     {
         return false;
     }
@@ -126,7 +127,7 @@ bool test(void)
     int testStringLength5 = 2;
     int testSubStringLength5 = 2;
     int answer5 = 1;
-    if (!(countingSubstrings(testString5, testSubstring5, testStringLength5, testSubStringLength5) == answer5))
+    if (!(countSubstrings(testString5, testSubstring5, testStringLength5, testSubStringLength5) == answer5))
     {
         return false;
     }
