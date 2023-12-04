b61413e16a37963b11a57b972be8e8f4d51b749e
kamendov-maxim <kamendov.maksim@icloud.com>
2023-12-04 14:41:45 +0300

    -

--- semester_1/homework4/mostFrequentElement2/smartQuickSort.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdbool.h>
-
-// smart quick sort main function
-void smartQuickSort(int array[], int leftElement, int rightElement);
-
-// a function for testing the work of quick sort
-bool testSmartQuickSort(void);
