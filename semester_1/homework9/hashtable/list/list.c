3b200e59d0700b56697870d86511c3d97d741f79
kamendov-maxim <kamendov.maksim@icloud.com>
2023-12-07 19:32:55 +0300

    list, string

--- semester_1/homework9/hashtable/list/list.c
+++ semester_1/homework9/hashtable/list/list.c
@@ -1,4 +1,8 @@
+#include <stdlib.h>
+#include <string.h>
+
 #include "list.h"
+#include "../String/String.h"
 
 typedef struct Node
 {
@@ -13,7 +17,81 @@ typedef struct List
     Node *root;
 } List;
 
-ListErrorCode append(List * const list, char * const value, const bool copyRecuired)
+List *createList(void)
 {
+    return calloc(1, sizeof(List));
+}
 
+ListErrorCode append(List * const list, char * const value, const bool copyRequired)
+{
+    char *valueCopy = value;
+    if (copyRequired)
+    {
+        valueCopy = copyString(value);
+        if (valueCopy == NULL)
+        {
+            return memoryErrorList;
+        }
+    }
+
+    Node *newNode = (Node *)calloc(1, sizeof(Node));
+    if (newNode == NULL)
+    {
+        free(valueCopy);
+        return memoryErrorList;
+    }
+    newNode->value = valueCopy;
+
+    if (list->root == NULL)
+    {
+        list->root = newNode;
+        return okList;
+    }
+
+    Node **currentNode = &(list->root);
+    for (; *currentNode != NULL; currentNode = &((*currentNode)->next));
+
+    *currentNode = newNode;
+    
+    return okList;    
+}
+
+bool checkElement(List const * const list, char const * const value)
+{
+    for (Node * currentNode = list->root; currentNode != NULL; currentNode = currentNode->next)
+    {
+        if (strcmp(value, currentNode->value) == 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void deleteElement(List * const list, char const * const value)
+{
+    for (Node **currentNode = &(list->root); *currentNode != NULL; currentNode = &((*currentNode)->next))
+    {
+        if (strcmp(value, (*currentNode)->value) == 0)
+        {
+            free((*currentNode)->value);
+            Node *nextTmp = (*currentNode)->next;
+            free(*currentNode);
+            *currentNode = nextTmp;
+            return;
+        }
+    }
+}
+
+void deleteList(List * const list)
+{
+    Node **currentNode = &(list->root);
+    while (*currentNode != NULL)
+    {
+        Node *nextTmp = (*currentNode)->value;
+        free((*currentNode)->value);
+        free(*currentNode);
+        *currentNode = nextTmp;
+    }
+    free(list);
 }
--- semester_1/homework9/hashtable/list/list.h
+++ semester_1/homework9/hashtable/list/list.h
@@ -6,5 +6,10 @@ typedef enum ListErrorCode
     memoryErrorList
 } ListErrorCode;
 
-typedef List List;
+typedef struct List List;
 
+List *createList(void);
+ListErrorCode append(List * const list, char * const value, const bool copyRequired);
+bool checkElement(List const * const list, char const * const value);
+void deleteElement(List * const list, char const * const value);
+void deleteList(List * const list);
