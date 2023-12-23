f4999dd7ee18bce310aa3a2d6ccafa0c6b09dca6
kamendov-maxim <kamendov.maksim@icloud.com>
2023-12-23 11:17:24 +0300

    Исправления

--- semester_1/homework8/AVLTree/tests/test.c
+++ semester_1/homework8/AVLTree/tests/test.c
@@ -179,28 +179,32 @@ static const bool testCase(size_t testLen, UserInput const *const testCommands,
 
 const bool test(void)
 {
-    UserInput testCommands1[5] = {appendCommand, appendCommand, deleteElementCommand, checkKeyCommand, getValueCommand};
+    UserInput testCommands1[5] = {appendCommand, appendCommand, deleteElementCommand,
+                                  checkKeyCommand, getValueCommand};
     char *testKeys1[5] = {"five", "six", "six", "six", "five"};
     char *testValues1[2] = {"five", "six"};
     char const *charAnswers1[1] = {"five"};
     bool const boolAnswers1[] = {false};
     bool testCase1 = testCase(5, testCommands1, testKeys1, testValues1, charAnswers1, boolAnswers1);
 
-    UserInput testCommands2[5] = {checkKeyCommand, appendCommand, checkKeyCommand, deleteElementCommand, checkKeyCommand};
+    UserInput testCommands2[5] = {checkKeyCommand, appendCommand, checkKeyCommand,
+                                  deleteElementCommand, checkKeyCommand};
     char *testKeys2[5] = {"one", "one", "one", "one", "one"};
     char *testValues2[1] = {"one"};
     char const *charAnswers2[0] = {};
     bool const boolAnswers2[3] = {false, true, false};
     bool testCase2 = testCase(5, testCommands2, testKeys2, testValues2, charAnswers2, boolAnswers2);
 
-    UserInput testCommands3[7] = {appendCommand, getValueCommand, appendCommand, getValueCommand, deleteElementCommand, appendCommand, getValueCommand};
+    UserInput testCommands3[7] = {appendCommand, getValueCommand, appendCommand,
+                                  getValueCommand, deleteElementCommand, appendCommand, getValueCommand};
     char *testKeys3[7] = {"one", "one", "one", "one", "one", "one", "one"};
     char *testValues3[3] = {"one", "two", "one"};
     char const *charAnswers3[3] = {"one", "two", "one"};
     bool const boolAnswers3[0] = {};
     bool testCase3 = testCase(4, testCommands3, testKeys3, testValues3, charAnswers3, boolAnswers3);
 
-    UserInput testCommands4[7] = {appendCommand, appendCommand, appendCommand, appendCommand, appendCommand, appendCommand, appendCommand};
+    UserInput testCommands4[7] = {appendCommand, appendCommand, appendCommand, appendCommand,
+                                  appendCommand, appendCommand, appendCommand};
     char *testKeys4[7] = {"1", "2", "3", "4", "5", "6", "7"};
     char *testValues4[7] = {"1", "2", "3", "4", "5", "6", "7"};
     char const *charAnswers4[0] = {};
