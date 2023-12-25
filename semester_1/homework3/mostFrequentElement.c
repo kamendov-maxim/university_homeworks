64ce4633c42c5f41746ac6f761e6b9afb6338406
kamendov-maxim <kamendov.maksim@icloud.com>
2023-12-25 12:37:39 +0300

    Исправления

--- semester_1/homework3/mostFrequentElement.c
+++ semester_1/homework3/mostFrequentElement.c
@@ -1,74 +1,22 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <time.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-int partition(int array[], int leftElement, int rightElement);
-void swap(int *firstValue, int *secondValue);
-int insertSort(int array[], int leftElement, int rightElement);
-int smartQuickSort(int array[], int leftElement, int rightElement);
-int mostFrequentElement(int array[], int size);
-
-bool test(void);
-bool testSmartQuickSort(void);
-bool testPartitionFunction(void);
-bool testSwapFunction(void);
-bool testStarterForPartitionFunction(int testArray[], int answerArray[], int size);
-bool testStarterForMostFrequentELement(int testArray[], int size, int answer);
-bool testMostFrequentElementFunction(void);
-
-int main()
+typedef enum ExitCode
 {
-    if (!test())
-    {
-        printf("\nSorry but the program does not work correctly\n");
-        return 1;
-    }
+    programFinishedCorrectly,
+    programFailedTests,
+    memoryError,
+    inputError
+} ExitCode;
 
-    printf("\nEnter the size of your array: ");
-    int size = 0;
-    scanf("%d", &size);
-
-    while (size <= 0)
-    {
-        printf("\nSize of your array should be at least 1\n");
-        printf("Enter the size of your array: ");
-        scanf("%d", &size);
-    }
-
-    int *array = malloc(size * sizeof(int));
-    if (array == NULL)
-    {
-        printf("\nSorry but something went wrong\n");
-        return 1;
-    }
-
-    printf("Enter numbers of your array one by one each other on the next line: \n");
-
-    for (int i = 0; i < size; ++i)
-    {
-        printf("Enter %d number of your array: ", i + 1);
-        int currentNumber = 0;
-        scanf("%d", &currentNumber);
-        array[i] = currentNumber;
-    }
-
-    int errorCode = smartQuickSort(array, 0, size - 1);
-    if (errorCode != 0)
-    {
-        printf("\nSorry but something went wrong\n");
-        return 1;
-    }
-
-    printf("\nThe most frequent element in the array is %d\n", mostFrequentElement(array, size));
-
-    return 0;
-}
-
-int mostFrequentElement(int array[], int size)
+static int mostFrequentElement(int const *const array, const size_t size)
 {
     int mostFrequentElement = array[0];
-    int counter = 1;
-    int maxCount = 1;
+    size_t counter = 1;
+    size_t maxCount = 1;
     for (int i = 1; i < size; ++i)
     {
         if (array[i] == array[i - 1])
@@ -88,59 +36,29 @@ int mostFrequentElement(int array[], int size)
     return mostFrequentElement;
 }
 
-int smartQuickSort(int array[], int leftElement, int rightElement)
+static void printArray(int const *const array, const size_t size)
 {
-    if (rightElement < leftElement)
+    for (size_t i = 0; i < size; ++i)
     {
-        return 2;
+        printf("%d ", array[i]);
     }
-
-    if (rightElement - leftElement + 1 < 10)
-    {
-        int errorCode = insertSort(array, leftElement, rightElement + 1);
-        if (errorCode != 0)
-        {
-            return 1;
-        }
-        return 0;
-    }
-    if (leftElement < rightElement)
-    {
-        int currentSeparator = partition(array, 0, rightElement);
-        smartQuickSort(array, leftElement, currentSeparator - 1);
-        smartQuickSort(array, currentSeparator + 1, rightElement);
-    }
-    return 0;
+    printf("\n");
 }
 
-int insertSort(int array[], int leftElement, int rightElement)
+static void swap(int *const firstValue, int *const secondValue)
 {
-    if (rightElement < leftElement)
-    {
-        return 1;
-    }
-
-    for (int i = leftElement; i < rightElement; ++i)
-    {
-        int currentElement = array[i];
-        int j = i - 1;
-        while (j >= leftElement && array[j] >= currentElement)
-        {
-            array[j + 1] = array[j];
-            --j;
-        }
-        array[j + 1] = currentElement;
-    }
-    return 0;
+    int buffer = *firstValue;
+    *firstValue = *secondValue;
+    *secondValue = buffer;
 }
 
-int partition(int array[], int leftElement, int rightElement)
+static size_t partition(int *const array, size_t leftElement, size_t rightElement)
 {
     rightElement -= 1;
     int currentSeparator = array[leftElement];
-    int i = rightElement;
+    size_t i = rightElement;
 
-    for (int j = rightElement; j > leftElement; --j)
+    for (size_t j = rightElement; j > leftElement; --j)
     {
         if (array[j] >= currentSeparator)
         {
@@ -154,136 +72,180 @@ int partition(int array[], int leftElement, int rightElement)
     return i;
 }
 
-void swap(int *firstValue, int *secondValue)
+static void insertSort(int *const array, size_t rightElement)
 {
-    int buffer = *firstValue;
-    *firstValue = *secondValue;
-    *secondValue = buffer;
-}
-
-bool test(void)
-{
-    if (!(testSwapFunction() * testPartitionFunction() * testSmartQuickSort() * testMostFrequentElementFunction()))
+    size_t leftElement = 0;
+    for (size_t i = leftElement + 1; i < rightElement; ++i)
     {
-        return false;
+        int currentElement = array[i];
+        int j = i - 1;
+        bool f = false;
+        while (j >= leftElement && array[j] >= currentElement)
+        {
+            array[j + 1] = array[j];
+            f = true;
+            if (j == 0)
+            {
+                break;
+            }
+            --j;
+        }
+        if (f)
+        {
+            array[j] = currentElement;
+        }
     }
-
-    return true;
 }
 
-bool testSwapFunction(void)
+static void smartQuickSort(int *const array, size_t leftElement, size_t rightElement)
+{
+    if (rightElement - leftElement + 1 < 10)
+    {
+        insertSort(array, rightElement + 1);
+        return;
+    }
+    if (leftElement < rightElement)
+    {
+        int currentSeparator = partition(array, 0, rightElement);
+        smartQuickSort(array, leftElement, currentSeparator - 1);
+        smartQuickSort(array, currentSeparator + 1, rightElement);
+    }
+}
+
+static const bool testSwapFunction(void)
 {
     int firstValue = 0;
     int secondValue = 1;
     swap(&firstValue, &secondValue);
-    if (firstValue != 1 || secondValue != 0)
-    {
-        return false;
-    }
-    return true;
+    return firstValue == 1 && secondValue == 0;
 }
 
-bool testStarterForArrays(int testArray[], int answerArray[], int size)
+static const bool testPartitionFunction(void)
 {
-    for (int i = 0; i < size; ++i)
+    const size_t testSize1 = 9;
+    int testArray1[testSize1] = {61, 99, 60, 80, 27, 57, 35, 81, 80};
+    int const answerArray1[testSize1] = {27, 57, 60, 35, 61, 99, 80, 81, 80};
+
+    const size_t testSize2 = 6;
+    int testArray2[testSize2] = {43, 36, 55, 60, 94, 24};
+    int const answerArray2[testSize2] = {24, 36, 43, 55, 60, 94};
+
+    const size_t testSize3 = 5;
+    int testArray3[testSize3] = {87, 91, 53, 37, 42};
+    int const answerArray3[testSize3] = {37, 42, 53, 87, 91};
+
+    const size_t testSize4 = 1;
+    int testArray4[testSize4] = {5};
+    int const answerArray4[testSize4] = {5};
+
+    partition(testArray1, 0, testSize1);
+    partition(testArray2, 0, testSize2);
+    partition(testArray3, 0, testSize3);
+    partition(testArray4, 0, testSize4);
+
+    return memcmp(testArray1, answerArray1, testSize1) == 0
+    && memcmp(testArray2, answerArray2, testSize2) == 0
+    && memcmp(testArray3, answerArray3, testSize3) == 0
+    && memcmp(testArray4, answerArray4, testSize4) == 0;
+}
+
+static const bool testSmartQuickSort(void)
+{
+    const size_t testSize1 = 5;
+    int testArray1[testSize1] = {5, 4, 3, 2, 1};
+    int answerArray1[testSize1] = {1, 2, 3, 4, 5};
+
+    const size_t testSize2 = 5;
+    int testArray2[testSize2] = {1, 2, 3, 4, 5};
+    int answerArray2[testSize2] = {1, 2, 3, 4, 5};
+
+    const size_t testSize3 = 1;
+    int testArray3[testSize3] = {1};
+    int answerArray3[testSize3] = {1};
+
+    smartQuickSort(testArray1, 0, testSize1 - 1);
+    smartQuickSort(testArray2, 0, testSize2 - 1);
+    smartQuickSort(testArray3, 0, testSize3 - 1);
+
+    return memcmp(testArray1, answerArray1, testSize1) == 0
+    && memcmp(testArray2, answerArray2, testSize2) == 0
+    && memcmp(testArray3, answerArray3, testSize3) == 0;
+}
+
+static const bool testMostFrequentElement(void)
+{
+    const size_t testSize1 = 10;
+    const int testArray1[testSize1] = {1, 3, 4, 4, 5, 6, 7, 7, 7, 8};
+    const int answer1 = 7;
+
+    const size_t testSize2 = 4;
+    const int testArray2[testSize2] = {3, 0, 0, 5};
+    const int answer2 = 0;
+
+    const size_t testSize3 = 1;
+    const int testArray3[testSize3] = {0};
+    const int answer3 = 0;
+
+    return mostFrequentElement(testArray1, testSize1) == answer1
+    && mostFrequentElement(testArray2, testSize2) == answer2
+    && mostFrequentElement(testArray3, testSize3) == answer3;
+}
+
+const bool test(void)
+{
+    return testSwapFunction()
+    && testPartitionFunction()
+    && testSmartQuickSort()
+    && testMostFrequentElement();
+}
+
+ExitCode main()
+{
+    if (!test())
     {
-        if (testArray[i] != answerArray[i])
+        printf("\nSorry but the program does not work correctly\n");
+        return programFailedTests;
+    }
+
+    size_t size = 0;
+    while (size == 0)
+    {
+        printf("Enter the size of your array: ");
+        if (scanf("%zd", &size) != 1)
         {
-            return false;
+            printf("Input error\n");
+            return inputError;
+        }
+        if (size == 0)
+        {
+            printf("\nSize of your array should be at least 1\n");
         }
     }
-    return true;
-}
 
-bool testPartitionFunction(void)
-{
-    int testArray1[9] = {61, 99, 60, 80, 27, 57, 35, 81, 80};
-    int answerArray1[9] = {27, 57, 60, 35, 61, 99, 80, 81, 80};
-
-    int testArray2[6] = {43, 36, 55, 60, 94, 24};
-    int answerArray2[6] = {24, 36, 43, 55, 60, 94};
-
-    int testArray3[5] = {87, 91, 53, 37, 42};
-    int answerArray3[5] = {37, 42, 53, 87, 91};
-
-    int testArray4[1] = {5};
-    int answerArray4[1] = {5};
-
-    partition(testArray1, 0, 9);
-    partition(testArray2, 0, 6);
-    partition(testArray3, 0, 5);
-    partition(testArray4, 0, 1);
-
-    if (!(testStarterForArrays(testArray1, answerArray1, 9) * testStarterForArrays(testArray2, answerArray2, 6) * testStarterForArrays(testArray3, answerArray3, 5) * testStarterForArrays(testArray4, answerArray4, 1)))
+    int *array = (int *)malloc(size * sizeof(int));
+    if (array == NULL)
     {
-        return false;
+        printf("\nNot enough memory\n");
+        return memoryError;
     }
 
-    return true;
-}
-
-bool testSmartQuickSort(void)
-{
-
-    int testArray1[5] = {5, 4, 3, 2, 1};
-    int answerArray1[5] = {1, 2, 3, 4, 5};
-
-    int testArray2[5] = {1, 2, 3, 4, 5};
-    int answerArray2[5] = {1, 2, 3, 4, 5};
-
-    int testArray3[1] = {1};
-    int answerArray3[1] = {1};
-
-    int errorCode = smartQuickSort(testArray1, 0, 5 - 1);
-    if (errorCode != 0)
+    printf("Enter numbers of your array one by one each other on the next line: \n");
+    for (size_t i = 0; i < size; ++i)
     {
-        return false;
+        printf("Enter %zd number of your array: ", i + 1);
+        int currentNumber = 0;
+        if (scanf("%d", &currentNumber) != 1)
+        {
+            printf("Input error\n");
+            return inputError;
+        }
+        array[i] = currentNumber;
     }
 
-    errorCode = smartQuickSort(testArray2, 0, 5 - 1);
-    if (errorCode != 0)
-    {
-        return false;
-    }
+    smartQuickSort(array, 0, size - 1);
 
-    errorCode = smartQuickSort(testArray3, 0, 1 - 1);
-    if (errorCode != 0)
-    {
-        return false;
-    }
+    printf("\nThe most frequent element in the array is %d\n", mostFrequentElement(array, size));
+    free(array);
 
-    if (!(testStarterForArrays(testArray1, answerArray1, 5) * testStarterForArrays(testArray2, answerArray2, 5) * testStarterForArrays(testArray3, answerArray3, 1)))
-    {
-        return false;
-    }
-
-    return true;
-}
-
-bool testMostFrequentElementFunction(void)
-{
-    int testArray1[10] = {1, 3, 4, 4, 5, 6, 7, 7, 7, 8};
-    int answer1 = 7;
-
-    int testArray2[4] = {3, 0, 0, 5};
-    int answer2 = 0;
-
-    int testArray3[1] = {0};
-    int answer3 = 0;
-
-    if (!(testStarterForMostFrequentELement(testArray1, 10, answer1) * testStarterForMostFrequentELement(testArray2, 4, answer2) * testStarterForMostFrequentELement(testArray3, 1, answer3)))
-    {
-        return false;
-    }
-
-    return true;
-}
-
-bool testStarterForMostFrequentELement(int testArray[], int size, int answer)
-{
-    if (mostFrequentElement(testArray, size) != answer)
-    {
-        return false;
-    }
-    return true;
+    return programFinishedCorrectly;
 }
